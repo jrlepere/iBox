58adcbf404dac39bc5fbd484c78c2058bca79b5b
jrlepere <jrlepere1@gmail.com>
2019-02-26 20:35:28 -0800

    integration test

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Run `java -jar iBox-x.x.x-SNAPSHOT-jar-with-dependencies.jar <path-to-config-fil
 
 ## Reporting
 Run `mvn site` to generate JaCoCo, CheckStyle and FindBugs reports, to be found in `target/site`.
+
+## Integration Tests
+1. Create or Modify the configuration file as defined in `config.json`.
+2. Modify `src/integration_test/java/com/jrlepere/iBox/iBoxIntegrationTest.java's CONFIG_FILE_PATH` to reflect step 1.
+3. Run `src/integration_test/java/com/jrlepere/iBox/iBoxIntegrationTest.java's CONFIG_FILE_PATH` as `JUnit`.
+Note: It was chosen to do the integration test in this way for two reasons. First, I did not want to provide my private key to the repository / circleci. Second, although it went untested (due to reason 1), I thought if I put the test in the src/test/java build path that mvn integration-test would fail on circleci for either internet errors or (definitely) missing configuration file.
--- src/integration_test/java/com/jrlepere/iBox/iBoxIntegrationTest.java
+++ src/integration_test/java/com/jrlepere/iBox/iBoxIntegrationTest.java
@@ -6,17 +6,17 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
 import java.util.HashMap;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.junit.Assert;
+import org.junit.Test;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.jrlepere.iBox.event_handlers.EventHandler;
@@ -24,33 +24,28 @@ import com.jrlepere.iBox.event_handlers.FileEventHandlerOrganizer;
 import com.jrlepere.iBox.event_handlers.file_handlers.AwsS3FileDelete;
 import com.jrlepere.iBox.event_handlers.file_handlers.AwsS3FileUpload;
 import com.jrlepere.iBox.event_handlers.file_handlers.FileHandler;
-import com.jrlepere.iBox.exceptions.WatchKeyGenerationException;
+import com.jrlepere.iBox.exceptions.WatchKeyResetException;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
+public class iBoxIntegrationTest {
 
-@Command(name = "iBox", mixinStandardHelpOptions = true)
-public class App implements Runnable {
+	private static final String CONFIG_FILE_PATH = "./.config.json";
+	private static final String TEMP_FILE_NAME = "temp.txt";
+	private static final int SLEEP_TIME_MILLISECONDS = 2500;
 	
-	@Parameters(paramLabel = "CONFIG_FILE", description = "Configuration File.")
-    private static File configFile;
-	
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void integratinTest() throws Exception {
 		
-		// parse command line arguments with picocli
-		CommandLine.run(new App(), args);
-		
-		JSONObject obj = (JSONObject) new JSONParser().parse(new FileReader(configFile)); 
-
+		// parse config file
+		JSONObject obj = (JSONObject) new JSONParser().parse(new FileReader(new File(CONFIG_FILE_PATH))); 
 		final File watchFolder = new File((String) obj.get("watchFolder"));
 		final String clientRegion = (String) obj.get("clientRegion");
 		final String accessKey = (String) obj.get("accessKey");
 		final String privateKey = (String) obj.get("privateKey");
 		final String bucketName = (String) obj.get("bucketName");
 		
+		// generate event handler
 		final Kind<?>[] events = {ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY};
-		final WatchKey watchKey = generateWatchKey(watchFolder.toPath(), events);
+		final WatchKey watchKey = App.generateWatchKey(watchFolder.toPath(), events);
 		final AmazonS3 s3Client = AwsS3FileUpload.createS3Client(clientRegion, accessKey, privateKey);
 		final EventHandler eventHandler = new FileEventHandlerOrganizer(
 				new HashMap<WatchEvent.Kind<?>, FileHandler>() {{
@@ -60,23 +55,40 @@ public class App implements Runnable {
 				}}
 		);
 		
+		// create folder listener
 		FolderListener folderListener = new FolderListener(watchKey, eventHandler, watchFolder);
 		
-		while (true) {
-			folderListener.checkForAndHandleEvents();
-			folderListener.resetKey();
-		}
+		// temporary file to create and delete
+		File tempFile = new File(watchFolder, TEMP_FILE_NAME);
 		
-    }
-
-	public static WatchKey generateWatchKey(Path dir, Kind<?>[] events) throws WatchKeyGenerationException {
-		try {
-			WatchService watcher = FileSystems.getDefault().newWatchService();
-			return dir.register(watcher, events);
-		} catch (IOException e) {
-			throw new WatchKeyGenerationException("Could not generate the watch key w/ exception: " + e.getMessage());
-		}
+		// test create
+		createFile(tempFile);
+		Thread.sleep(SLEEP_TIME_MILLISECONDS);
+		listen(folderListener);
+		Thread.sleep(SLEEP_TIME_MILLISECONDS);
+		Assert.assertTrue(s3Client.doesObjectExist(bucketName, TEMP_FILE_NAME));
+		
+		// test delete
+		deleteFile(tempFile);
+		Thread.sleep(SLEEP_TIME_MILLISECONDS);
+		listen(folderListener);
+		Thread.sleep(SLEEP_TIME_MILLISECONDS);
+		Assert.assertFalse(s3Client.doesObjectExist(bucketName, TEMP_FILE_NAME));
+		
 	}
 	
-	public void run() {}
+	private void createFile(File tempFile) throws IOException {
+		FileWriter tempFileWriter = new FileWriter(tempFile);
+		tempFileWriter.close();
+	}
+	
+	private void deleteFile(File tempFile) {
+		tempFile.delete();
+	}
+	
+	private void listen(FolderListener folderListener) throws WatchKeyResetException {
+		folderListener.checkForAndHandleEvents();
+		folderListener.resetKey();
+	}
+	
 }
